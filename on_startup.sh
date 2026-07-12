c526f091ca7c4db178c90be2604a29e815c67039
William Ma <mawilliam.v20@gmail.com>
2026-07-12 18:36:03 -0400

    Revert "Claude/chemcpa docker troubleshoot rywrjk"

--- on_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# RESTORED-UPSTREAM(williamrobotma/chemCPA): verbatim HF Space build-context file
-# (b1ro/chemCPA), restored into the repo; not a fork modification.
-# Write some commands here that will run on root user before startup.
-# For example, to clone transformers and install it in dev mode:
-# git clone https://github.com/huggingface/transformers.git
-# cd transformers && pip install -e ".[dev]"
